bacc3e2189405792787865d1befe5e456d1695f6
Dave Allie <dave@daveallie.com>
2017-02-22 22:28:42 +1000

    Cleanup monkey patching. Throw if Stamp isn't defined, don't define it

--- lib/stamp-l10n.rb
+++ lib/stamp-l10n.rb
@@ -7,26 +7,10 @@ require 'stamp-l10n/translator_extension'
 require 'stamp-l10n/emitters/am_pm_extension'
 require 'stamp-l10n/emitters/lookup_extension'
 
-module Stamp
-  prepend StampL10n::StampExtension
+::Stamp.include StampL10n::StampExtension
+::Stamp::Translator.include StampL10n::TranslatorExtension
+::Stamp::Emitters::AmPm.extend StampL10n::Emitters::AmPmExtension::ClassMethods
+::Stamp::Emitters::Lookup.extend StampL10n::Emitters::LookupExtension::ClassMethods
 
-  class Translator
-    prepend StampL10n::TranslatorExtension
-  end
-
-  module Emitters
-    class AmPm
-      class << self
-        prepend StampL10n::Emitters::AmPmExtension::ClassMethods
-      end
-    end
-    class Lookup
-      class << self
-        prepend StampL10n::Emitters::LookupExtension::ClassMethods
-      end
-    end
-  end
-end
-
-Date.send(:include, Stamp)
-Time.send(:include, Stamp)
+Date.include Stamp
+Time.include Stamp
